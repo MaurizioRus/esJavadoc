98ee7767fb8c65f96fbafa9029536c43f0f345f6
MaurizioRus <mauriziocovelli9@gmail.com>
2023-08-01 19:27:55 +0200

    first

--- Tester.java
+++ Tester.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Tester {
+/*create a class called Tester that has the main method where you:
+create a new WordGames object
+print in console the result of the addHelloWord method invocation
+print in console the result of the getFullName method invocation
+ */
+
+/**
+ * Tester class of WordGames class
+ * Author : Maurizio
+ */
+public class Tester {
+    public static void main(String[] args) {
+
+        WordGames wordGames = new WordGames(); //creating a WordGames object
+
+        String helloWord1 = wordGames.addHelloWord("hello"); //invocation of addHelloWord method
+        System.out.println(helloWord1); //print of the helloWord1 string
+
+        String fullName = wordGames.getFullName("Mario" , "Bros");  //invocation of fullname method
+
+    }
 }
--- WordGames.java
+++ WordGames.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class WordGames {
+/*Create a class called WordGames that has two methods:
+addHelloWord method that takes one parameter as input called word and returns the String Hello [wordHere]
+getFullName method that takes two parameters as input called name and surname and returns their concatenation
+ */
+
+/**
+ * This class manipulate Words
+ * Author : Maurizio
+ */
+public class WordGames {
+
+    /**
+     * This function takes a String parameter and return hello + the parameter
+     * @param word
+     * @return
+     */
+    public String addHelloWord(String word){
+        return "hello " + word;
+    }
+
+    /**
+     * This function takes name and surname and return their concatenation
+     * @param name
+     * @param surname
+     * @return
+     */
+    public String getFullName(String name , String surname){
+
+        return name + " " + surname;
+    }
 }
